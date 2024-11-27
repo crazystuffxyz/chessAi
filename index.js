66ce192b2b10e3b6a931014134ef1cf1794f163b
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2024-11-27 13:16:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -298,7 +298,7 @@ chessAIFunctions.reloadChessEngine = function() {
 
 // Loads the chess engine by initializing a WebSocket connection
 chessAIFunctions.loadChessEngine = function() {
-  const socketUrl = 'ws://localhost:8080'; // Replace with your actual WebSocket URL
+  const socketUrl = 'wss://chessai-server.onrender.com'; // Replace with your actual WebSocket URL
   engine.engine.initializeSocket(socketUrl);
   console.log("Loaded chess engine");
 };
